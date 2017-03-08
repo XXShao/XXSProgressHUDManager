0104e9ac56e03ee98769c959c711e467378189c8
zhaoxiang <zhaoxiang@xingzhi.com>
2017-03-08 10:11:09 +0800

    上传文件

---
+++ ZX_MBProgressManager/OpenSource/ViewController/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ZX_MBProgressManager
+//
+//  Created by 赵祥 on 17/3/4.
+//  Copyright © 2017年 XZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+
+
+@end
+
